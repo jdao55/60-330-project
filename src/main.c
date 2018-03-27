746aa58c626b25d1cdb75a434d8aaf943b0b91e5
joedao <joedao55@gmail.com>
2018-03-27 19:55:00 -0400

    updated makefile

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ void print_output_stats(int tlb_hit, int page_table_miss);
 
 int main()
 {
-    FILE * address_file=fopen("addesses.txt", "r");
+    FILE * address_file=fopen("addresses.txt", "r");
     char physical_memory[128][256];
     int physical_memory_status[128];//whether spot in memory is empty
     size_t i, page_fault_count=0, tlb_hit_count=0;
@@ -26,10 +26,11 @@ int main()
 
     lru_stack *tlb_table=construct_lru(16);
     lru_stack *page_table=construct_lru(128);
-    
+    int err;
     for(i=0;i<1000;i++)
     {
-        fscanf(address_file,"%d\n", &address);
+        err=fscanf(address_file,"%d\n", &address);
+        if(err<0) perror("Error with file IO");
         offset=address & 0xFF;
         page_num=address>>8;
         //check tlb
@@ -68,12 +69,14 @@ int find_memory_space(int mem_status[128])
 
 void load_memory(int page_num,lru_stack* ptable,int mem_status[],char phys_memory[][256])
 {
-    FILE * backing_store=fopen("backingstore.dat", "w");
+    FILE * backing_store=fopen("BACKING_STORE.bin", "r");
     char buffer[256];
     int mem_space;
-
-    fseek(backing_store, page_num*256, SEEK_SET);
-    fread(buffer,1, 256,backing_store);
+    int err;
+    err=fseek(backing_store, page_num*256, SEEK_SET);
+    err=fread(buffer,1, 256,backing_store);
+    if(err<0)
+        perror("Error with file IO");
     fclose(backing_store);
     
     //find free memspace if one exists
@@ -85,6 +88,7 @@ void load_memory(int page_num,lru_stack* ptable,int mem_status[],char phys_memor
     //copy to phsy mem
     memcpy(phys_memory[mem_space], buffer, 256);
     //update page table
+
     node * new_node=construct_node(page_num, mem_space, NULL, NULL);
     push(ptable, new_node);
 }
@@ -99,4 +103,15 @@ int check_lru(int page_num, lru_stack *tlb)
     }
     else
         return 0;
+}
+
+void print_output(int addr,int frame, int offset, char memory[][256])
+{
+    int phys_addr=(frame<<8)|offset;
+    printf("Virtual address: %d Physical address: %d Value: %d", addr, phys_addr, memory[frame][offset]);
+}
+
+void print_output_stats(int tlb_hit, int page_table_miss)
+{
+    printf("TLB HITS: %d page miss: %d",tlb_hit,page_table_miss);
 }
